05fd5e7c2c044f4fb6b3193c84aa9a95ba252e0d
Arhor <DKY8pdej>
2019-11-26 00:49:29 +0300

    Micronaut basic app added;

---
+++ micronaut/src/main/java/by/arhor/microservice/Application.java
@@ -0,0 +1,11 @@
+package by.arhor.microservice;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+  public static void main(String[] args) {
+    Micronaut.run(Application.class);
+  }
+
+}
